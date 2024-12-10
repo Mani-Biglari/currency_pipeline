ddde3dea79b43d6729ded41212dc97aafe78f88c
ManiBiglari <mani.biglari@icloud.com>
2024-12-10 21:11:05 +0300

    removed unused files

---
+++ prefect_deployment.py
@@ -0,0 +1,14 @@
+from prefect import flow
+
+# Source for the code to deploy (here, a GitHub repo)
+SOURCE_REPO="https://github.com/Mani-Biglari/currency_pipeline.git"
+
+if __name__ == "__main__":
+    flow.from_source(
+        source=SOURCE_REPO,
+        entrypoint="flow.py:currency_pipeline_flow", # Specific flow to run
+    ).deploy(
+        name="first-deployment",
+        work_pool_name="currency_pipeline_pool", # Work pool target
+        cron="* * * * *", # Cron schedule (every minute)
+    )
